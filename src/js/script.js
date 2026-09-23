be87f71ee292c0cfe08168df416492d3a1a15a45
Julia Silva <jusilvadev@gmail.com>
2026-09-23 01:09:09 -0300

    feat: adiciona prompts para cadastro do vinho

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+let nomeVinho = prompt("Qual é o nome do vinho?");
+let tipoVinho = prompt("Qual é o tipo do vinho? (Tinto, Branco ou Rosé)");
+let safra = prompt("Qual é a safra do vinho");
+let quantidadeEstoque = prompt("Qual é a quantidade em estoque?");
+
